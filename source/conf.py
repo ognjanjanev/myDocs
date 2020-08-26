e68d0862ece2de5cd13d515fc61fed4e24ee42d8
Ognjan Janev <ognjan.janev@gmail.com>
2020-08-26 07:20:28 +0200

    setup: add githubpages extension to conf.py

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,8 @@ release = '0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
